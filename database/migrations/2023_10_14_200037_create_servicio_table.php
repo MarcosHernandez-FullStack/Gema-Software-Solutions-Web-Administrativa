249ad51644cc5cabea1e8b51494e0135ea1e31ad
marcos-programadorUNT <mhernandezb@unitru.edu.pe>
2023-10-19 15:13:34 -0500

    Marcos 19/10/2023 15:13

--- database/migrations/2023_10_14_200037_create_servicio_table.php
+++ database/migrations/2023_10_14_200037_create_servicio_table.php
@@ -15,10 +15,10 @@ class CreateServicioTable extends Migration
     {
         Schema::create('servicio', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre')->unique();
+            $table->string('nombre');
             $table->enum('estado', ['ACTIVO', 'INACTIVO'])->default('ACTIVO');
-            $table->string('ruta_foto_principal')->nullable();
-            $table->string('ruta_foto_secundaria')->nullable();
+            $table->string('ruta_foto_principal');
+            $table->string('ruta_foto_secundaria');
             $table->string('descripcion_resumida');
             $table->text('descripcion_amplia');
            /*  $table->foreignId('users_id')->constrained('users')->nullable(); */
