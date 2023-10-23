9998ef049adecf61641f5430c1daee95cd63cc51
gustavopimol <67304817+gustavopimol@users.noreply.github.com>
2023-10-23 18:58:22 -0500

    "Actualizacion 23-10-2023 18:58"

--- app/Http/Controllers/Api/ApiWebInformativaController.php
+++ app/Http/Controllers/Api/ApiWebInformativaController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Proyecto;
 use Illuminate\Http\Request;
 use App\Models\Servicio;
-use App\Models\SubServicio;
 use Illuminate\Support\Facades\Storage;
 
 class ApiWebInformativaController extends Controller
@@ -13,7 +13,7 @@ class ApiWebInformativaController extends Controller
 
     public function getServicioAll()
     {
-        $servicios = Servicio::where('estado', '=', 'ACTIVO')->get();
+        $servicios = Servicio::where('estado', '=', '1')->get();
         return response()->json([
             'res' => true,
             'servicios' => $servicios,
@@ -24,8 +24,8 @@ class ApiWebInformativaController extends Controller
     /* Alternativa */
     public function getHome()
     {
-        $servicios = Servicio::where('estado', '=', 'ACTIVO')->get();
-        $ultimosProyectos = SubServicio::where('estado', '=', 'ACTIVO')->orderBy('id', 'desc')->get();
+        $servicios = Servicio::where('estado', '=', '1')->get();
+        $ultimosProyectos = Proyecto::where('estado', '=', '1')->orderBy('id', 'desc')->get();
 
 
         return response()->json([
@@ -33,12 +33,12 @@ class ApiWebInformativaController extends Controller
             'servicios' => $servicios->map(function ($servicio) {
                 return [
                     "id" => $servicio->id,
-                    "descripcion" => $servicio->descripcion,
-                    "detalle_descripcion_resumida" => $servicio->detalle_descripcion_resumida,
-                    "detalle_descripcion_amplia" => $servicio->detalle_descripcion_amplia,
+                    "nombre" => $servicio->nombre,
+                    "descripcion_resumida" => $servicio->descripcion_resumida,
+                    "descripcion_amplia" => $servicio->descripcion_amplia,
                     "estado" => $servicio->estado,
                     "ruta_foto_principal" =>  env("APP_URL") . Storage::url($servicio->ruta_foto_principal),
-                    "ruta_foto_detalle" =>  env("APP_URL") . Storage::url($servicio->ruta_foto_detalle),
+                    "ruta_foto_secundaria" =>  env("APP_URL") . Storage::url($servicio->ruta_foto_secundaria),
 
                     /* "ruta_foto" => env("APP_URL") . "storage/" . $servicio->ruta_foto, */
                 ];
@@ -47,13 +47,37 @@ class ApiWebInformativaController extends Controller
                 $fecha_implementacion = date('m/Y', strtotime($proyecto->fecha_implementacion));
                 return [
                     "id" => $proyecto->id,
-                    "descripcion" => $proyecto->descripcion,
+                    "nombre" => $proyecto->nombre,
                     "servicio_id" => $proyecto->servicio->id,
-                    "servicio" => $proyecto->servicio->descripcion,
-                    "empresa_cliente" => $proyecto->empresa_cliente,
+                    //"empresa_id" => $proyecto->empresa_id,
                     "fecha_implementacion" => $fecha_implementacion,
                     "estado" => $proyecto->estado,
                     "ruta_foto" =>  env("APP_URL") . Storage::url($proyecto->ruta_foto),
+                    
+                    "servicio" => $proyecto->servicio->nombre,
+                    "empresa_cliente" => $proyecto->empresa->razon_social,
+                    "sub_servicio_detalle" => $proyecto->detalles_proyecto->map(function ($detalle) {
+                        return [
+                            "id" => $detalle->id,
+                            "estado" => $detalle->estado,
+                            "nombre" => $detalle->nombre,
+                            "ruta_foto" =>   env("APP_URL") . Storage::url($detalle->ruta_foto),
+                            "proyecto_id"=>$detalle->proyecto_id,
+                        ];
+                    }),
+
+                    /*"id" => $proyecto->id,
+                    "nombre" => $proyecto->nombre,
+                    "servicio_id" => $proyecto->servicio->id,
+                    //"empresa_id" => $proyecto->empresa_id,
+                    "fecha_implementacion" => $fecha_implementacion,
+                    "estado" => $proyecto->estado,
+                    "ruta_foto" =>  env("APP_URL") . Storage::url($proyecto->ruta_foto),
+                    
+                    "servicio" => $proyecto->servicio->descripcion,
+                    "empresa_cliente" => $proyecto->empresa_cliente,
+                    
+                    
 
                     "sub_servicio_detalle" => $proyecto->sub_servicio_detalles->map(function ($detalle) {
                         return [
@@ -62,7 +86,7 @@ class ApiWebInformativaController extends Controller
                             "descripcion" => $detalle->descripcion,
                             "ruta_foto" =>   env("APP_URL") . Storage::url($detalle->ruta_foto),
                         ];
-                    }),
+                    }),*/
 
 
                     /* "ruta_foto" => env("APP_URL") . "storage/" . $servicio->ruta_foto, */
@@ -74,8 +98,8 @@ class ApiWebInformativaController extends Controller
     public function getServicioPorId($id)
     {
         $servicio = Servicio::findOrFail($id);
-        $listaServicios = Servicio::where('estado', '=', 'ACTIVO')->get();
-        $ultimosProyectos = SubServicio::where('estado', '=', 'ACTIVO')
+        $listaServicios = Servicio::where('estado', '=', '1')->get();
+        $ultimosProyectos = Proyecto::where('estado', '=', '1')
             ->where('servicio_id', '=', $id)
             ->orderBy('id', 'desc')->get();
 
@@ -83,11 +107,11 @@ class ApiWebInformativaController extends Controller
         return response()->json([
             "servicio" => [
                 "id" => $servicio->id,
-                "descripcion" => $servicio->descripcion,
-                "detalle_descripcion_amplia" => $servicio->detalle_descripcion_amplia,
+                "nombre" => $servicio->nombre,
+                "descripcion_amplia" => $servicio->descripcion_amplia,
                 "estado" => $servicio->estado,
                 "ruta_foto_principal" =>  env("APP_URL") . Storage::url($servicio->ruta_foto_principal),
-                "ruta_foto_detalle" =>  env("APP_URL") . Storage::url($servicio->ruta_foto_detalle),
+                "ruta_foto_secundaria" =>  env("APP_URL") . Storage::url($servicio->ruta_foto_secundaria),
                 "beneficios" => $servicio->beneficios
             ],
 
@@ -95,7 +119,7 @@ class ApiWebInformativaController extends Controller
                 $bEstado = $servicioItem->id === $servicio->id; // Compara los IDs
                 return [
                     "id" => $servicioItem->id,
-                    "descripcion" => $servicioItem->descripcion,
+                    "nombre" => $servicioItem->nombre,
                     "estado" => $servicioItem->estado,
                     "bEstado" => $bEstado,
                 ];
@@ -105,14 +129,14 @@ class ApiWebInformativaController extends Controller
                 $fecha_implementacion = date('m/Y', strtotime($proyecto->fecha_implementacion));
                 return [
                     "id" => $proyecto->id,
-                    "descripcion" => $proyecto->descripcion,
-                    "servicio" => $proyecto->servicio->descripcion,
-                    "empresa_cliente" => $proyecto->empresa_cliente,
+                    "nombre" => $proyecto->nombre,
+                    "servicio" => $proyecto->servicio->nombre,
+                    "empresa_cliente" => $proyecto->empresa->razon_social,
                     "fecha_implementacion" => $fecha_implementacion,
                     "estado" => $proyecto->estado,
                     "ruta_foto" =>  env("APP_URL") . Storage::url($proyecto->ruta_foto),
 
-                    "sub_servicio_detalle" => $proyecto->sub_servicio_detalles->map(function ($detalle) {
+                    "sub_servicio_detalle" => $proyecto->detalles_proyecto->map(function ($detalle) {
                         return [
                             "id" => $detalle->id,
                             "estado" => $detalle->estado,
