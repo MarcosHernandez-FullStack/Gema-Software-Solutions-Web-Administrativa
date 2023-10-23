c0d1d82fc7025e01e08cce97aa96d25ae0032f10
Elpredicador123 <agentepredicador@gmail.com>
2023-10-23 10:31:24 -0500

    mejora cards

--- resources/views/livewire/diseño/diseño-component.blade.php
+++ resources/views/livewire/diseño/diseño-component.blade.php
@@ -17,7 +17,8 @@
                         @endif
                         <div class="row my-1 d-flex justify-content-between align-items-center">
                             <div class="row col-sm-12 col-md-6">
-                                <div class="border-info border-bottom text-info h1 font-weight-bolder" style="border-bottom-width:5px !important">Diseño Cards</div>
+                                <div class="border-info border-bottom text-info h1 font-weight-bolder"
+                                    style="border-bottom-width:5px !important">Diseño Cards</div>
                             </div>
                             <div class="row col-sm-12 col-md-2 justify-content-end">
                                 <button type="button" class="btn btn-info btn-sm rounded-pill" data-toggle="modal"
@@ -30,63 +31,70 @@
                         <div class="row my-1">
                             <div class="form-group col-sm-12 col-md-4 d-flex align-items-center"><label
                                     class="m-0">Search</label><input type="search"
-                                    class="form-control form-control-sm ml-1 rounded-pill" placeholder="" aria-controls="example1">
+                                    class="form-control form-control-sm ml-1 rounded-pill" placeholder=""
+                                    aria-controls="example1">
                             </div>
                         </div>
                         <div class="row my-1">
 
                             @forelse ($servicios as $key => $servicio)
-                            <div class="col-md-4">
-                                <!-- Widget: user widget style 1 -->
-                                <div class="card card-widget widget-user shadow-lg">
-                                  <!-- Add the bg color to the header using any of the bg-* classes -->
-                                  <div class="widget-user-header text-white"
-                                       style="background: url('{{Storage::url($servicio->ruta_foto_principal)}}') center center;">
-                                    <div class="text-right">
-                                        <span role="button"
-                                        class="badge rounded-pill text-sm bg-{{ $servicio->estado == '1' ? 'success' : 'warning' }}" wire:click='cambiarEstado({{ $servicio->id }})'>{{ $servicio->estado == '1' ? 'ACTIVO' : 'INACTIVO' }}</span>
-                                    </div>
-                                  </div>
-                                  <div class="widget-user-image">
-                                    <img class="img-circle" style="height: 90px;object-fit: cover;" src="{{Storage::url($servicio->ruta_foto_secundaria)}}" alt="{{$servicio->descripcion_resumida}}">
-                                  </div>
-                                  <div class="p-4">
-                                    <div class="row my-2">
-                                        <div class="d-flex flex-column justify-content-between col-6">
-                                            <h5 class="widget-user-desc text-left text-md" >{{ $servicio->descripcion_resumida }}</h5>
+                                <div class="col-md-4 px-5">
+                                    <!-- Widget: user widget style 1 -->
+                                    <div class="card card-widget widget-user shadow-lg" style="border-radius: 25px">
+                                        <!-- Add the bg color to the header using any of the bg-* classes -->
+                                        <div class="widget-user-header text-white"
+                                            style="background: url('{{ Storage::url($servicio->ruta_foto_principal) }}') center center;border-radius: 25px 25px 0 0;">
+                                            <div class="text-right">
+                                                <span role="button"
+                                                    class="badge rounded-pill text-sm bg-{{ $servicio->estado == '1' ? 'success' : 'warning' }}"
+                                                    wire:click='cambiarEstado({{ $servicio->id }})'>{{ $servicio->estado == '1' ? 'ACTIVO' : 'INACTIVO' }}</span>
+                                            </div>
+                                        </div>
+                                        <div class="widget-user-image">
+                                            <img class="img-circle" style="height: 90px;object-fit: cover;"
+                                                src="{{ Storage::url($servicio->ruta_foto_secundaria) }}"
+                                                alt="{{ $servicio->descripcion_resumida }}">
+                                        </div>
+                                        <div class="p-5">
                                             <div class="row">
-                                                <button type="button" class="btn btn-sm rounded-pill bg-info" wire:click='rediregirProyectos({{ $servicio->id }})'><i class="fas fa-project-diagram"></i> PROYECTOS</button>
-                                                <button type="button" class="btn btn-sm btn-warning btn-sm rounded-pill" data-toggle="modal"
-                                                    data-target="#modal_usuario" wire:click="edit({{ $servicio->id }})"><i class="fas fa-pen"></i> 
-                                                    EDITAR
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div class="col-6 text-md">
-                                            <div>
-                                                <h5>Beneficios...</h5>
-                                                <ul>
-                                                @forelse ($servicio->beneficios as $beneficio)
-                                                    <li>{{$beneficio->descripcion}}</li>
-                                                    @empty
-                                                    <li>:3</li>
-                                                    @endforelse
-                                                </ul>
+                                                <div class="col-12 my-2">
+                                                    <h5 class="widget-user-desc text-left text-md">
+                                                        {{ $servicio->descripcion_resumida }}</h5>
+                                                </div>
+                                                <div class="col-12 text-md my-2">
+                                                    <div>
+                                                        <h5><strong>Beneficios</strong></h5>
+                                                        <div class="">
+                                                            @forelse ($servicio->beneficios as $beneficio)
+                                                                <div>{{ $beneficio->descripcion }}</div>
+                                                            @empty
+                                                                <div>:3</div>
+                                                            @endforelse
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 my-2">
+                                                    <div class="d-flex justify-content-between">
+                                                        <button type="button" class="btn btn-sm rounded-pill bg-info"
+                                                            wire:click='rediregirProyectos({{ $servicio->id }})'><i
+                                                                class="fas fa-project-diagram"></i> PROYECTOS</button>
+                                                        <button type="button"
+                                                            class="btn btn-sm btn-warning btn-sm rounded-pill"
+                                                            data-toggle="modal" data-target="#modal_usuario"
+                                                            wire:click="edit({{ $servicio->id }})"><i
+                                                                class="fas fa-pen"></i>
+                                                            EDITAR
+                                                        </button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="row my-2">
-                                        <div class="">
-
-                                        </div>
-                                    </div>
-                                  </div>
+                                    <!-- /.widget-user -->
                                 </div>
-                                <!-- /.widget-user -->
-                              </div>
-                              @empty
-                              
-                              @endforelse
+                            @empty
+
+                            @endforelse
                         </div>
                         @include('layouts.footer-listado', ['servicios' => $servicios])
                     </div>
@@ -113,7 +121,8 @@
                         @endif
                         <div class="row my-1 d-flex justify-content-between align-items-center">
                             <div class="row col-sm-12 col-md-6">
-                                <div class="border-info border-bottom text-info h1 font-weight-bolder" style="border-bottom-width:5px !important">Diseño listado</div>
+                                <div class="border-info border-bottom text-info h1 font-weight-bolder"
+                                    style="border-bottom-width:5px !important">Diseño listado</div>
                             </div>
                             <div class="row col-sm-12 col-md-2 justify-content-end">
                                 <button type="button" class="btn btn-info btn-sm rounded-pill" data-toggle="modal"
@@ -126,12 +135,14 @@
                         <div class="row my-1">
                             <div class="form-group col-sm-12 col-md-4 d-flex align-items-center"><label
                                     class="m-0">Search</label><input type="search"
-                                    class="form-control form-control-sm ml-1 rounded-pill" placeholder="" aria-controls="example1">
+                                    class="form-control form-control-sm ml-1 rounded-pill" placeholder=""
+                                    aria-controls="example1">
                             </div>
                         </div>
                         <div class="row my-1">
                             <div class="col-sm-12">
-                                <table id="example1" class="table table-sm m-0 rounded-pill" aria-describedby="example1_info">
+                                <table id="example1" class="table table-sm m-0 rounded-pill"
+                                    aria-describedby="example1_info">
                                     <thead>
                                         <tr>
                                             <th rowspan="1" colspan="1">#</th>
@@ -145,18 +156,22 @@
                                         @forelse ($servicios as $key => $servicio)
                                             <tr>
                                                 <td>{{ $key + 1 }}</td>
-                                                <td><img src="{{Storage::url($servicio->ruta_foto_principal)}}" class="img-thumbnail w-25" alt="no_hay_imagen"></td>
+                                                <td><img src="{{ Storage::url($servicio->ruta_foto_principal) }}"
+                                                        class="img-thumbnail w-25" alt="no_hay_imagen"></td>
                                                 <td>{{ $servicio->descripcion_resumida }}</td>
                                                 <td><span role="button"
-                                                        class="badge rounded-pill bg-{{ $servicio->estado == '1' ? 'success' : 'warning' }}" wire:click='cambiarEstado({{ $servicio->id }})'>{{ $servicio->estado == '1' ? 'ACTIVO' : 'INACTIVO' }}</span>
+                                                        class="badge rounded-pill bg-{{ $servicio->estado == '1' ? 'success' : 'warning' }}"
+                                                        wire:click='cambiarEstado({{ $servicio->id }})'>{{ $servicio->estado == '1' ? 'ACTIVO' : 'INACTIVO' }}</span>
                                                 </td>
                                                 <td><span role="button" class="badge rounded-pill bg-info"
-                                                        wire:click='rediregirProyectos({{ $servicio->id }})'>PROYECTOS</span></td>
-                                                <td><button type="button" class="btn btn-warning btn-sm rounded-pill" data-toggle="modal"
-                                                    data-target="#modal_usuario" wire:click="edit({{ $servicio->id }})">
-                                                    EDITAR
-                                                </button></td>
-                                                
+                                                        wire:click='rediregirProyectos({{ $servicio->id }})'>PROYECTOS</span>
+                                                </td>
+                                                <td><button type="button" class="btn btn-warning btn-sm rounded-pill"
+                                                        data-toggle="modal" data-target="#modal_usuario"
+                                                        wire:click="edit({{ $servicio->id }})">
+                                                        EDITAR
+                                                    </button></td>
+
                                             </tr>
                                         @empty
                                             <tr>
